1b7ced9abbd21b4e1a0e16b0687d84038a376447
sarahmr63089 <sarahrose63089@gmail.com>
2020-07-02 22:24:49 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,7 @@ class Show < ActiveRecord::Base
   belongs_to :network
 
   def actors_list
+    binding.pry
     characters_array = Character.where(show_id: self.id)
     actor_id_array = characters_array.map { |c| c.actor_id }
     actor_id_array.map { |id| Actor.find(id).full_name }
