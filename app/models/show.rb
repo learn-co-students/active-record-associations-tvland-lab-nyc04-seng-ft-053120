f219a0a79d194053cf264174a002a2b4b79369ef
tiggy31 <tigist_alemu@fitnyc.edu>
2020-07-04 21:09:14 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors
+   belongs_to :network
+
+   def actors_list
+      self.characters.map do |c|
+         "#{c.actor.full_name}"
+     # binding.pry
+      "#{c.actor.full_name}"
+      end
+   end
 end
